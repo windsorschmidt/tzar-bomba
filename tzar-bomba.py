d299b7d7687a9379cddfc8a635176748f86bad6b
Windsor Schmidt <windsor.schmidt@gmail.com>
2017-05-16 19:08:16 +0000

    copy style sheet to project directory at run time

--- tzar-bomba.py
+++ tzar-bomba.py
@@ -9,12 +9,14 @@
 #
 # /path/to/tzar-bomba.py "%I" "%O" /path/to/parts.sqlite
 #
-# For each schematic component, the value of custom field "part" is used to
-# extract details associated with that component from the sqlite3 database.
+# For each schematic component, the value of custom field "internal_part" is
+# an index to that component's detail record in the sqlite3 database.
 #
-# Following a successful run, a new bill-of-materials file should be waiting
-# for you in your project folder (e.g. project.csv).
+# Following a successful run, exported BOMs in CSV and HTML should be waiting
+# for you in your project folder (e.g. project.csv, project.html).
 
+import os
+from shutil import copyfile
 from collections import defaultdict
 from lxml import etree
 import sqlite3
@@ -57,6 +59,7 @@ db = sqlite3.connect(dbfile)
 cur = db.execute('select * from parts')
 header = ['Qty.', 'Reference(s)'] + \
          [str.title(row[0].replace('_', ' ')) for row in cur.description]
+
 for part in bom:
     line = [str(len(bom[part])), ', '.join(bom[part])]
     query = "SELECT * from parts where internal_part is '" + part + "'"
@@ -68,11 +71,12 @@ for part in bom:
 db.close()
 
 # Print a summary to the console (displayed by EESchema's BOM tool)
-print('Part references : {}'.format(len(components)))
-print('Line items      : {}'.format(len(line_items)))
-print('Unresolved refs : {}'.format(len(missing_refs)))
+print('{} line items'.format(len(line_items)))
+print('{} part references'.format(len(components)))
 if len(missing_refs):
-    print(', '.join(missing_refs))
+    print('{} {} {}'.format(len(missing_refs),
+                            'References without internal part field:',
+                            ', '.join(missing_refs)))
 
 # Save as comma-separated values
 
@@ -85,12 +89,18 @@ with open(outfile + '.csv', 'w') as csvfile:
 # Save as HTML
 
 project = etree.parse(infile).findall('design/sheet/title_block/title')[0].text
-title = 'Bill of Materials: ' + project
+version = etree.parse(infile).findall('design/sheet/title_block/rev')[0].text
+date = etree.parse(infile).findall('design/sheet/title_block/date')[0].text
+source = etree.parse(infile).findall('design/source')[0].text
+title = 'Bill of Materials: ' + project + ' v' + version
+
+displayed_fields = range(6)
 
 with open(outfile + '.html', 'w') as f:
     f.write('<!DOCTYPE html>\n')
     f.write('<html lang="en">\n')
     f.write('  <head>\n')
+    f.write('    <meta charset="utf-8"/>\n')
     f.write('    <title>' + title + '</title>\n')
     f.write('    <link rel="stylesheet" type="text/css" href="style.css">\n')
     f.write('  </head>\n')
@@ -98,14 +108,22 @@ with open(outfile + '.html', 'w') as f:
     f.write('    <h1>' + title + '</h1>\n')
     f.write('    <table>\n')
     f.write('      <tr>\n')
-    for col in header[:6]:
-        f.write('        <th>' + str(col).replace(' ', '&nbsp;') + '</th>\n')
+    for i, col in enumerate(header):
+        if i in displayed_fields:
+            f.write('        <th>'
+                    + str(col).replace(' ', '&nbsp;') + '</th>\n')
+        else:
+            f.write('        <th class="hide">' +
+                    str(col).replace(' ', '&nbsp;') + '</th>\n')
     f.write('      </tr>\n')
     for row in sorted(line_items, key=lambda line: line[1]):
-        datasheet = datasheet_dir + '/' + row[6] if row[6] else '#'
+        datasheet = datasheet_dir + '/' + row[6] if len(row) >= 6 else '#'
         f.write('      <tr>\n')
-        for i, col in enumerate(row[:6]):
-            f.write('        <td>')
+        for i, col in enumerate(row):
+            if i in displayed_fields:
+                f.write('        <td>')
+            else:
+                f.write('        <td class="hide">')
             if i == 3:
                 f.write('<a href="' + datasheet + '">' + str(col) + '</a>')
             else:
@@ -113,6 +131,16 @@ with open(outfile + '.html', 'w') as f:
             f.write('        </td>\n')
         f.write('      </tr>\n')
     f.write('    </table>\n')
+    f.write('  <p>Line items      : {}'.format(len(line_items)) + '<br>\n')
+    f.write('  Part references : {}'.format(len(components)) + '<br>\n')
+    if len(missing_refs):
+        f.write('  <strong>References without internal part field: ' +
+                ''.join(missing_refs) + '</strong><br>\n')
+    f.write('  Exported from ' + source + '</p>\n')
     f.write('  </body>\n')
     f.write('</html>\n')
     print('BOM written to ' + outfile + '.html')
+
+css_src = os.path.dirname(os.path.realpath(__file__)) + '/style.css'
+css_dst = str.join('/', outfile.split('/')[:-1]) + '/style.css'
+copyfile(css_src, css_dst)
